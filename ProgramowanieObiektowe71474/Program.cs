99673a9b3219743e87e18d8b609162eb2d6920b0
71474 <71474@student.wsiz.edu.pl>
2025-10-06 13:21:54 +0200

    testBranch

--- ProgramowanieObiektowe71474/Program.cs
+++ ProgramowanieObiektowe71474/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("No dzień dobry :0 ");
+Console.WriteLine("Test branch tekst ");
