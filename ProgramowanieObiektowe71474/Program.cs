9d90e75ab9fc5250a8ff14ac6ba035656530b7d7
WiniarskiP <128233769+hotuewa@users.noreply.github.com>
2025-10-09 16:00:52 +0200

    Delete ProgramowanieObiektowe71474 directory

--- ProgramowanieObiektowe71474/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("No dzień dobry :0 ");
-Console.WriteLine(" Zmieniony tekst elo");
